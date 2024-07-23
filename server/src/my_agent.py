7d77004d417fb3b9b025afd51c4997062d58d3d7
Reed Bender <reedbndr@gmail.com>
2024-07-22 22:27:35 -0500

    update agent, done with testing

--- server/src/my_agent.py
+++ server/src/my_agent.py
@@ -83,9 +83,7 @@ class MyAgent:
         ### EXECUTE AGENT
         print(f"==> Executing agent...")
         chat_thread.append({'role': 'user', 'content': user_json['question']})
-        # agent_results, agent_metadata = self.invoke_gpt_agent(chat_thread)
-        return self.invoke_gpt_agent(chat_thread)
-
+        agent_results, agent_metadata = self.invoke_gpt_agent(chat_thread)
 
         ### STREAM RESPONSE WITH PERSONA
         final_response, persona_metadata = self.notebook_persona_generation(
@@ -180,9 +178,7 @@ class MyAgent:
             selected_tools=selected_tools
         )
         total_cost += planning_metadata['cost']
-        
-        return generated_plan
-         
+                 
         #########################################################
         ### 2 --> EXECUTE STEP 1 OF THE PLAN IN REDIS WORKERS ###
         #########################################################
